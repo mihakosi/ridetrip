e5bf3f8dbdc9ddc3fb1ff8f03c5805f875c3e139
Miha <mihakosi@icloud.com>
2020-07-09 14:39:58 +0200

    Add Docker configuration

--- index.js
+++ index.js
@@ -5,43 +5,72 @@ var path = require("path");
 
 const app = express();
 
-require("./api/models/db");
+const pg = require("pg");
 
-const api = require("./api/routes/index");
+const setup = () => {
+  require("./api/models/db");
 
-app.use(express.json());
+  const api = require("./api/routes/index");
 
-app.use("/api", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
+  app.use(express.json());
 
-app.use("/api", api);
+  app.use("/api", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
-app.use(express.static(path.join(__dirname, "app", "build")));
+  app.use("/api", api);
 
-app.get("*", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "app", "build", "index.html"));
-});
+  app.use(express.static(path.join(__dirname, "app", "build")));
 
-app.use("/api", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Cache-Control", "max-age=3600");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With", "Content-Type", "Accept", "Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+  app.get("*", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "app", "build", "index.html"));
+  });
 
-app.use((error, req, res, next) => {
-  if (error.name == "UnauthorizedError") {
-    res.status(401).json({
-      message: "Please sign in to access this resource.",
-    });
-  }
-});
+  app.use("/api", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Cache-Control", "max-age=3600");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With", "Content-Type", "Accept", "Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+  });
 
-app.listen(process.env.PORT || 3000);
+  app.use((error, req, res, next) => {
+    if (error.name == "UnauthorizedError") {
+      res.status(401).json({
+        message: "Za dostop je potrebna prijava.",
+      });
+    }
+  });
 
-console.log("Server started");
+  app.listen(process.env.PORT || 3000);
+
+  console.log("Server started");
+};
+
+var client = new pg.Client(
+  `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE_DEFAULT}`
+);
+client.connect();
+client
+  .query(`SELECT FROM pg_database WHERE datname='${process.env.POSTGRES_DATABASE}'`)
+  .then((result) => {
+    if (result.rowCount == 0) {
+      client
+        .query(`CREATE DATABASE ${process.env.POSTGRES_DATABASE}`)
+        .then((result) => {
+          client.end();
+          setup();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      setup();
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
